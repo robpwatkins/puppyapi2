70b8679f944d5d34b1958f55234dfedca6299edf
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-07 10:32:13 -0500

    let that gooo

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,102 +11,109 @@ import PuppyPic from './PuppyPic';
 
 function Content() {
   return (
-    <div className="row">
-      <div className="column column1">
-        <PuppyPic pup={Bill} name="Winston"/>
-        <PuppyPic pup={Brandy} name="Brandy" />
-      </div>
-      <div className="column">
-        <PuppyPic pup={Minnie} name="Minnie"/>
-        <PuppyPic pup={Blondie} name="Blondie" />
-      </div>
-      <div className="column column1">
-        <PuppyPic pup={Lady} name="Lady"/>
-        <PuppyPic pup={Sharky} name="Sharky" />
-      </div>
-      <div className="column">
-        <PuppyPic pup={Bill} name="Winston"/>
-        <PuppyPic pup={Brandy} name="Brandy" />
-      </div>
-    </div>
-
-
-
-
-
-
-
-    // <Grid 
-    //   container 
-    //   // justify="center"
-    //   direction="row"
-    //   wrap 
-    //   // spacing={2}
-    //   justify="space-evenly"
-    //   style={{paddingTop: "16px"}}
-    //   >
-    //   <Grid 
-    //     container 
-    //     item 
-    //     direction="column" 
-    //     spacing={2}
-    //     xs={12} 
-    //     sm={6}
-    //     md={3}>
-    //     <Grid item>
-    //       <PupCard pup={Bill} name="Winston"/>
-    //     </Grid>
-    //     <Grid item>
-    //       <PupCard pup={Blondie} name="Bondie" />
-    //     </Grid>
+    // <Grid container>
+    //   <Grid item xs={12} sm={6} md={4} lg={3}>
+    //     <PupCard pup={Bill} name="Winston"/>
     //   </Grid>
-    //   <Grid 
-    //     container 
-    //     direction="column" 
-    //     spacing={2}
-    //     wrap 
-    //     item 
-    //     xs={12} 
-    //     sm={6}
-    //     md={3}>
-    //     <Grid item>
-    //       <PupCard pup={Minnie} name="Minnie"/>
-    //     </Grid>
-    //     <Grid item>
-    //       <PupCard pup={Sharky} name="Sharky" />
-    //     </Grid>
+    //   <Grid item xs={12} sm={6} md={4} lg={3}>
+    //     <PupCard pup={Brandy} name="Brandy"/>
     //   </Grid>
-    //   <Grid 
-    //     container 
-    //     direction="column" 
-    //     spacing={2}
-    //     wrap 
-    //     item 
-    //     xs={12} 
-    //     sm={6}
-    //     md={3}>
-    //     <Grid item>
-    //       <PupCard pup={Lady} name="Lady" />
-    //     </Grid>
-    //     <Grid item>
-    //     </Grid>
+    //   <Grid item xs={12} sm={6} md={4} lg={3}>
+    //     <PupCard pup={Lady} name="Lady"/>
     //   </Grid>
-    //   <Grid 
-    //     container 
-    //     direction="column" 
-    //     spacing={2}
-    //     wrap 
-    //     item 
-    //     xs={8} 
-    //     sm={6}
-    //     md={3}>
-    //     <Grid item>
-    //       <PupCard pup={Brandy} name="Brandy" />
-    //     </Grid>
-    //     <Grid item>
-    //     </Grid>
+    //   <Grid item xs={12} sm={6} md={4} lg={3}>
+    //     <PupCard pup={Lady} name="Lady"/>
     //   </Grid>
     // </Grid>
+
+
+
+
+
+
+
+
+
+
+    // <div className="row">
+    //   <div className="column column1">
+    //     <PuppyPic pup={Bill} name="Winston"/>
+    //     <PuppyPic pup={Brandy} name="Brandy" />
+    //   </div>
+    //   <div className="column">
+    //     <PuppyPic pup={Minnie} name="Minnie"/>
+    //     <PuppyPic pup={Blondie} name="Blondie" />
+    //   </div>
+    //   <div className="column column1">
+    //     <PuppyPic pup={Lady} name="Lady"/>
+    //     <PuppyPic pup={Sharky} name="Sharky" />
+    //   </div>
+    //   <div className="column">
+    //     <PuppyPic pup={Bill} name="Winston"/>
+    //     <PuppyPic pup={Brandy} name="Brandy" />
+    //   </div>
+    // </div>
+
+
+
+
+
+
+
+    <Grid 
+      container 
+      // justify="center"
+      direction="row"
+      wrap 
+      // spacing={2}
+      justify="space-evenly"
+      style={{paddingTop: "16px"}}
+      >
+      <Grid 
+        container 
+        item 
+        direction="column" 
+        spacing={2}
+        xs={12} 
+        sm={4}>
+        <Grid item>
+          <PupCard pup={Bill} name="Winston"/>
+        </Grid>
+        <Grid item>
+          <PupCard pup={Brandy} name="Brandy" />
+        </Grid>
+      </Grid>
+      <Grid 
+        container 
+        direction="column" 
+        spacing={2}
+        wrap 
+        item 
+        xs={12} 
+        sm={4}>
+        <Grid item>
+          <PupCard pup={Minnie} name="Minnie"/>
+        </Grid>
+        <Grid item>
+          <PupCard pup={Blondie} name="Blondie" />
+        </Grid>
+      </Grid>
+      <Grid 
+        container 
+        direction="column" 
+        spacing={2}
+        wrap 
+        item 
+        xs={12} 
+        sm={4}>
+        <Grid item>
+          <PupCard pup={Lady} name="Lady" />
+        </Grid>
+        <Grid item>
+          <PupCard pup={Sharky} name="Sharky" />
+        </Grid>
+      </Grid>
+    </Grid>
 
 
 
