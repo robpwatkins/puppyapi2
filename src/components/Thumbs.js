a6dfe73c07d0d13273e150f4d285e0426f08cec6
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-10 13:33:35 -0500

    added Narcissa. ten puppersss

--- src/components/Thumbs.js
+++ src/components/Thumbs.js
@@ -8,6 +8,7 @@ import Sharky from '../img/Sharky-thumb.png';
 import Max from '../img/Max-thumb.jpg';
 import Heidi from '../img/Heidi-thumb.jpg';
 import Libby from '../img/Libby-thumb.jpg';
+import Narcissa from '../img/Narcissa-thumb.jpg';
 import PupCard from './PupCard';
 import { Grid } from '@material-ui/core';
 
@@ -42,58 +43,7 @@ function Thumbs() {
         <PupCard pup={Libby} name="Libby" />
       </Grid>
       <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Winston} name="Winston" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Minnie} name="Minnie" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Lady} name="Lady" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Blondie} name="Blondie" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Brandy} name="Brandy" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Sharky} name="Sharky" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Max} name="Max" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Heidi} name="Heidi" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Libby} name="Libby" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Winston} name="Winston" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Minnie} name="Minnie" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Lady} name="Lady" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Blondie} name="Blondie" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Brandy} name="Brandy" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Sharky} name="Sharky" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Max} name="Max" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Heidi} name="Heidi" />
-      </Grid>
-      <Grid container item xs={4} sm={3} md={2} lg={1}>
-        <PupCard pup={Libby} name="Libby" />
+        <PupCard pup={Narcissa} name="Narcissa" />
       </Grid>
     </Grid>
   )
